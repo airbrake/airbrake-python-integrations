afb55f4de7c4c0fde161bac27c2d7cb3d263fb3e
Zach Goldstein <zachgold@gmail.com>
2017-08-11 15:06:59 -0400

    Pep8 fixups in __about__

--- airbrake_python_integrations/__about__.py
+++ airbrake_python_integrations/__about__.py
@@ -1,27 +1,27 @@
 """Airbrake-Python package attributes and metadata."""
 
 __all__ = (
-  '__title__',
-  '__summary__',
-  '__author__',
-  '__email__',
-  '__version__',
-  '__keywords__',
-  '__license__',
-  '__url__',
-  '__notifier__',
+    '__title__',
+    '__summary__',
+    '__author__',
+    '__email__',
+    '__version__',
+    '__keywords__',
+    '__license__',
+    '__url__',
+    '__notifier__',
 )
 
 __title__ = 'airbrake-python-integrations'
 __summary__ = 'Python Integrations for airbrake.io'
 __author__ = 'Zach Goldstein'
 __email__ = 'zach@airbrake.io'
-__version__ = '1.0'
+__version__ = '1.0.0'
 __keywords__ = ['airbrake', 'exceptions', 'airbrake.io']
 __license__ = 'Apache License, Version 2.0'
 __url__ = 'https://github.com/airbrake/airbrake-python-integrations'
 __notifier__ = {
-  'name': 'airbrake-python',
-  'version': __version__,
-  'url': __url__,
+    'name': 'airbrake-python',
+    'version': __version__,
+    'url': __url__,
 }
