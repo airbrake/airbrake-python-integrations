4f9c5140e69d775682411ec456c49aa9f31b150d
Zach Goldstein <zachgold@gmail.com>
2017-08-11 15:06:28 -0400

    Fix imports in flask example

--- examples/flask/example.py
+++ examples/flask/example.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from airbrake_python_integrations.flask.airbrake import AirbrakeApp
+from airbrake_python_integrations.flask.app import AirbrakeApp
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
-ab = Airbrake(app)
+ab = AirbrakeApp(app)
 
 
 @app.route('/')
