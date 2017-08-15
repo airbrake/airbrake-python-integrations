cfc85f13de5e444ee6994e834e539a5871a0c2d9
Zach Goldstein <zachgold@gmail.com>
2017-08-15 14:55:52 -0400

    pep8 tab fixups

--- tests/django/test_logger.py
+++ tests/django/test_logger.py
@@ -9,25 +9,26 @@ from django.core.exceptions import PermissionDenied
 
 from airbrake.notifier import Airbrake
 
+
 class AirbrakeMiddlewareTestCase(TestCase):
 
-  def setUp(self):
-    self.request = HttpRequest()
-    self.response = HttpResponse()
-    self.logger = airbrake.getLogger(
-        api_key='fakekey', project_id='fakeprojectid')
+    def setUp(self):
+        self.request = HttpRequest()
+        self.response = HttpResponse()
+        self.logger = airbrake.getLogger(
+            api_key='fakekey', project_id='fakeprojectid')
 
-  def view_log_exception(self, err_text, body):
-    self.logger.error(err_text)
-    return HttpResponse(body)
+    def view_log_exception(self, err_text, body):
+        self.logger.error(err_text)
+        return HttpResponse(body)
 
-  @mock.patch.object(Airbrake, "notify")
-  def test_logging_exception(self, notify):
-    err_text = "Test Error"
-    body = "<html><body>I am a banana.</body></html>"
-    resp = self.view_log_exception(err_text, body)
-    self.assertTrue(resp.content, body)
-    exception = notify.call_args[0][0]
-    self.assertTrue(isinstance(exception, Notice))
-    self.assertEqual(exception.errors[0]["message"], err_text)
-    self.assertEqual(exception.errors[0]["severity"], "error")
+    @mock.patch.object(Airbrake, "notify")
+    def test_logging_exception(self, notify):
+        err_text = "Test Error"
+        body = "<html><body>I am a banana.</body></html>"
+        resp = self.view_log_exception(err_text, body)
+        self.assertTrue(resp.content, body)
+        exception = notify.call_args[0][0]
+        self.assertTrue(isinstance(exception, Notice))
+        self.assertEqual(exception.errors[0]["message"], err_text)
+        self.assertEqual(exception.errors[0]["severity"], "error")
--- tests/django/test_middleware.py
+++ tests/django/test_middleware.py
@@ -7,20 +7,20 @@ from django.core.exceptions import PermissionDenied
 
 from ab.django.middleware import AirbrakeNotifierMiddleware
 from airbrake.notifier import Airbrake
-# from tests.conf import AIRBRAKE, DATABASES
+
 
 class AirbrakeMiddlewareTestCase(TestCase):
 
-  def setUp(self):
-    # django.setup()
-    self.ab_middleware = AirbrakeNotifierMiddleware()
-    self.request = HttpRequest()
-    self.response = HttpResponse()
+    def setUp(self):
+        # django.setup()
+        self.ab_middleware = AirbrakeNotifierMiddleware()
+        self.request = HttpRequest()
+        self.response = HttpResponse()
 
-  @mock.patch.object(Airbrake, "notify")
-  def test_middleware_exception(self, notify):
-    exception = PermissionDenied("test_err")
-    self.ab_middleware.process_exception(self.request, exception)
-    exception = notify.call_args[0][0]
-    self.assertTrue(str(exception) == "test_err")
-    self.assertTrue(isinstance(exception, PermissionDenied))
+    @mock.patch.object(Airbrake, "notify")
+    def test_middleware_exception(self, notify):
+        exception = PermissionDenied("test_err")
+        self.ab_middleware.process_exception(self.request, exception)
+        exception = notify.call_args[0][0]
+        self.assertTrue(str(exception) == "test_err")
+        self.assertTrue(isinstance(exception, PermissionDenied))
